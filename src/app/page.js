9921117905b1b306bdc45b318a32dd7c82f973e2
Jacques <24863947@sun.ac.za>
2023-10-25 22:17:24 +0200

    New page idea

--- src/app/page.js
+++ src/app/page.js
@@ -11,46 +11,18 @@ import { CustomCursor } from "@/lib/custom-cursor";
 const SidebarOpenContext = createContext(null);
 
 export default function Home() {
-    // to toggle menu sidebar globally
-    const [menuOpen, setMenuOpen] = useState(false);
-
     return (
-        <SidebarOpenContext.Provider value={{menuOpen, setMenuOpen}}>
-            <main className={`${inter.variable} ${lora.variable}`}>
-                <Nav />
-                <Landing />
-                <span className="rocket-animation"><Rocket /></span>
-            </main>
-            <CustomCursor />
-        </SidebarOpenContext.Provider>
+        <main className={`${inter.variable} ${lora.variable} grid grid-cols-2 grid-rows-2 p-24`}>
+            <div><AnimatedHeader /></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </main>
     )
 }
 
-export function Nav() {
-    const title = "Jacques Marais";
-
-    // get/set/toggle sidebar's global open state
-    const { menuOpen, setMenuOpen } = useContext(SidebarOpenContext);
-    const toggleSidebar = () => setMenuOpen(!menuOpen);
-
-    return (
-        <>
-            <nav className={`p-6 flex items-center gap-4 absolute z-40 transition-all ${menuOpen ? 'text-black pt-8': ''}`}>
-                <button className={`transition-transform ${menuOpen ? 'rotate-180': 'rotate-0'} transition-all relative top-0 hover:-top-[2px] group`} onClick={toggleSidebar}><ChevronDownCircle className={`${menuOpen ? 'group-hover:fill-black group-hover:stroke-white': 'group-hover:fill-white group-hover:stroke-sky-950'} transition-all`} size="32" /></button>
-                {/* <h5>{title.toUpperCase()}</h5> */}
-            </nav>
-            <nav className={`bg-white w-full z-30 absolute top-0 p-8 pl-[5.5rem] transition-transform ${menuOpen ? 'translate-y-0 text-black': '-translate-y-full text-white'}`}>
-                <ul className="flex gap-8 text-lg relative top-[2px]">
-                    <li className={`top-nav-item ${menuOpen ? 'left-0 opacity-100': '-left-4 opacity-0'}`}><a href="/projects">{/*<ChevronRight size={16} strokeWidth={4} />*/}Projects</a></li>
-                    <li className={`top-nav-item ${menuOpen ? 'left-0 opacity-100': '-left-8 opacity-0'}`}><a href="/contact">{/*<ChevronRight size={16} strokeWidth={4} />*/}Contact</a></li>
-                </ul>
-            </nav>
-        </>
-    );
-}
-
 export function AnimatedHeader() {
-    const text = "I build software and develop front-ends.";
+    const text = "This is Jacques Marais.";
     const words = text.split(" ");
 
     const [highlightedIndex, setHighlightedIndex] = useState(0);
@@ -76,14 +48,6 @@ export function AnimatedHeader() {
     }, []);
     
     return (
-        <h1 className='text-white/70 xl font-extrabold tracking-normal uppercase w-11/12'>{ spans }</h1>
-    )
-}
-
-export function Landing() {
-    return (
-        <section className="pt-32">
-            <AnimatedHeader />
-        </section>
+        <h1 className='text-white/70 text-8xl font-extrabold tracking-normal uppercase'>{ spans }</h1>
     )
 }
